b2c682e36570d81c8845140a9f6a5cc240abdfe8
Diogo Magliano <diogo.magliano@zup.com.br>
2021-12-07 11:16:45 -0300

    Dependencia All Open e Mockito para testes

--- src/main/kotlin/br/com/zup/livros/Livro.kt
+++ src/main/kotlin/br/com/zup/livros/Livro.kt
@@ -2,14 +2,20 @@ package br.com.zup.livros
 
 import com.fasterxml.jackson.annotation.JsonFormat
 import java.time.LocalDate
+import javax.persistence.Entity
+import javax.persistence.GeneratedValue
+import javax.persistence.GenerationType
+import javax.persistence.Id
 
-data class LivrosRequestDto(
+@Entity
+class Livro(
     val titulo: String,
     val isbn: String,
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
     val dataPublicacao: LocalDate
 ) {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    var id : Long? = null
 
-
-    fun toLivro() = Livro(titulo, isbn, dataPublicacao)
 }
--- src/main/kotlin/br/com/zup/tickets/DetalheTicketResponse.kt
+++ src/main/kotlin/br/com/zup/tickets/DetalheTicketResponse.kt
@@ -1,14 +1,12 @@
 package br.com.zup.tickets
 
-import com.fasterxml.jackson.annotation.JsonFormat
-import java.time.LocalDateTime
+import io.micronaut.core.annotation.Introspected
 
-class DetalheTicketResponse(ticket: Ticket) {
-    val id: Long? = ticket.id
-    val titulo: String = ticket.titulo
-    val descricao: String = ticket.descricao
-    val placaVeiculo: String = ticket.placaVeiculo
-    val status: StatusTicketsEnum = ticket.status
-    @JsonFormat(shape=JsonFormat.Shape.STRING, pattern="yyyy-MM-dd'T'HH:mm:ss", timezone="UTC-03")
-    val dataCriacao: LocalDateTime = ticket.dataCriacao
+@Introspected
+class DetalheTicketResponse(
+    val titulo: String,
+    val placaVeiculo: String,
+    val status: StatusTicketsEnum,
+) {
+
 }
